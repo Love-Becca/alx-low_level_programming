2094dc636bbf7692290fc9c4e5338d418c2724c4
Love-Becca <follycube2020@gmail.com>
2022-10-18 14:37:59 -0700

    no such thing as absolute task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @c: takes a parameter to be passed
+ * Return: always 0
+ */
+
+int _abs(int c)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
