ccab0f2b53465d30feb72c9014687581675ef16f
Love-Becca <follycube2020@gmail.com>
2022-10-20 12:32:17 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - function that pritn a square followed by a new line
+ * @size: parameter
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int c, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (c = 0; c < size; c++)
+		{
+			for (b = 0; b < c; b++)
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
