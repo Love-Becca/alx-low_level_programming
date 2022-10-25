7319e7f4b5bfce2d673346ca01c4b702c07e8feb
Love-Becca <follycube2020@gmail.com>
2022-10-24 19:31:22 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: parameter
+ * Return:0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*str != '\0')
+		i++;
+
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+
