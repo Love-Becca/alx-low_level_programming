4130f85c77de7a1bc49c27fc0b81407dac291f9c
Love-Becca <follycube2020@gmail.com>
2022-10-27 09:31:21 -0700

    string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @c: parameter
+ * Return: 0
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+
+		i++;
+	}
+	return (0);
+}
