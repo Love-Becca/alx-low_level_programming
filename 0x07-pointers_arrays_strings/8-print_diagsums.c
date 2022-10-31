4e7e25948c2043814d8ef44f2c9a8c90d4bb15b9
Love-Becca <follycube2020@gmail.com>
2022-10-31 14:28:17 -0700

    corrcetion

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,23 +5,22 @@
  * print_diagsums - print sum of diagonal
  * @a: parameter
  * @size: parameter
- * Return: 0
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i, one = 0, two = 0;
+	int i, sum_one = 0, sum_two = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		one += a[i];
+		sum_one += a[i];
 		a += size;
 	}
 	a -= size;
 	for (i = 0; i < size; i++)
 	{
-		two += a[i];
+		sum_two += a[i];
 		a -= size;
 	}
-	printf("%d, %d\n", one, two)
+	printf("%d, %d\n", sum_one, sum_two);
 }
