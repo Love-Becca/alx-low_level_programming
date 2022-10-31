68457873d18381f00dc3166361fcb07d19419970
Love-Becca <follycube2020@gmail.com>
2022-10-31 12:41:07 -0700

    memcpy correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *arr = dest;
+	int i;
 
-	while (n-- > 0)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
 	return (dest);
 }
