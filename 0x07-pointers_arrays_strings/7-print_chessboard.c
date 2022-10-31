44f6cae6892e4444ad30c0b65d0fd66153cf1d2d
Love-Becca <follycube2020@gmail.com>
2022-10-31 14:07:54 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print matrix
+ * @a: parameter
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+
+		}
+		_putchar('\n');
+	}
+}
