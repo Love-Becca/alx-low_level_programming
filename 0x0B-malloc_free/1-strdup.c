af1e305d86992d647fcaa00b92af267a0a94cb25
Love-Becca <follycube2020@gmail.com>
2022-11-08 03:23:50 -0800

    correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,6 +11,7 @@ char *_strdup(char *str)
 {
 	int i, k;
 	char *j;
+
 	i = 0;
 
 	if (str == NULL)
@@ -24,7 +25,7 @@ char *_strdup(char *str)
 	j = malloc((i * sizeof(char)) + 1);
 
 	if (j == NULL)
-		return(NULL);
+		return (NULL);
 	for (k = 0; str[k]; k++)
 		j[k] = str[k];
 	return (j);
