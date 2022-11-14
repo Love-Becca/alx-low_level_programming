b07005329ec3c1f09b5ab93a521227c6e988a114
Love-Becca <follycube2020@gmail.com>
2022-11-14 09:42:49 -0800

    dog loves

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - function that intialize struct
+ * @d: dog alias
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
