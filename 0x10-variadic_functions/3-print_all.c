55658f36d3efb330998b1be18ca126e00e6a617e
Love-Becca <follycube2020@gmail.com>
2022-11-18 08:02:02 -0800

    print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: print format
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list all;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int d = 0;
+	char *string;
+	const char save[] = "cifs";
+
+	va_start(all, format);
+	while (format && format[i])
+	{
+		while (save[j])
+		{
+			if (format[i] == save[j] && d)
+			{
+				printf(",");
+				break;
+			}
+			j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(all, int)), d = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(all, int)), d = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(all, double)), d = 1;
+				break;
+			case 's':
+				string = va_arg(all, char *), d = 1;
+				if (!string)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", string);
+				break;
+		}
+		i++;
+	}
+
+	printf("\n");
+	va_end(all);
+}
