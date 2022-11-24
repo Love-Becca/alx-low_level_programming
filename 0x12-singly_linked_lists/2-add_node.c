41cd19d8122d35cf0f1d6adf843661569225f341
Love-Becca <follycube2020@gmail.com>
2022-11-24 09:40:44 -0800

    add to node head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - add new nodes
+ * @head: node start
+ * @str: string
+ * Return: head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *duplicate;
+	int i;
+	list_t new_node;
+
+	new_node = mallloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	duplicate = strdup(str);
+
+	if (duplicate == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	for (i = 0; str[i];)
+		i++;
+	new_node->str = duplicate;
+	new_node->len = i;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
