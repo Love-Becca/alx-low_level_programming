62087f8278ab7f26c7b2ef554d97d58136e95527
Love-Becca <follycube2020@gmail.com>
2022-11-29 08:24:04 -0800

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head nodes
+ * @head: head node
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	int list;
+	listint_t *linked;
+	listint_t *save;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	linked = *head;
+
+	list = linked->n;
+
+	save = linked->next;
+
+	free(linked);
+
+	*head = save;
+	
+	return (list);
+}
