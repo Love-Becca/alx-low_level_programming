5b42e347994e13c39df8f1f2d1d180d8df3831b6
Love-Becca <follycube2020@gmail.com>
2022-11-28 17:45:06 -0800

    printlist

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements
+ * @h: parameter
+ * Return: count
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		i++;
+	}
+	return (i);
+}
