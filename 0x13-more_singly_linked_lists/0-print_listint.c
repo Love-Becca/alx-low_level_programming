74b23304f3951b084a6ae7b451ce740611384696
Love-Becca <follycube2020@gmail.com>
2022-11-28 18:12:36 -0800

    refactor

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while (h != NULL)
+	for (; h != NULL; h = h->next)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		i++;
 	}
 	return (i);
