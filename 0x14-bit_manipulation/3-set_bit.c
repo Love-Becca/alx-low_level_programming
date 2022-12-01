73d51133441b2a74a2e8983f044f6aa35d8c9876
Love-Becca <follycube2020@gmail.com>
2022-12-01 13:01:31 -0800

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * set_bit - set bit value
+ * @n: pointer
+ * @index: set index
+ * Return: int
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+	return (1);
+}
